b294552dbbd064ecd723bfcdd9a73f45b970e5e8
Trong-Vu <trong-vu.tran@student.uclouvain.be>
2018-11-15 14:58:47 +0100

    context_switching

--- tasks/riot/context_switching/main.c
+++ tasks/riot/context_switching/main.c
@@ -1,44 +1,49 @@
+#define INTERVAL (1U * US_PER_SEC)
 
 #include <stdio.h>
 #include "thread.h"
 #include "xtimer.h"
 #include "timex.h"
 
-#define INTERVAL (1U * US_PER_SEC)
+char threadA_stack[THREAD_STACKSIZE_MAIN];
+char threadB_stack[THREAD_STACKSIZE_MAIN];
 
-char thread1_stack[THREAD_STACKSIZE_MAIN];
-char thread2_stack[THREAD_STACKSIZE_MAIN];
-
-void *thread1(void *arg)
+void *threadA(void *arg)
 {
     (void) arg;
     xtimer_ticks32_t last_wakeup = xtimer_now();
 
     while(1) {
         xtimer_periodic_wakeup(&last_wakeup, INTERVAL);
-        puts("Thread 1");
+        puts("Thread A: echo");
     }
     return NULL;
 }
 
-void *thread2(void *arg)
+void *threadB(void *arg)
 {
     (void) arg;
-    while(1){}
+    xtimer_ticks32_t last_wakeup = xtimer_now();
+
+    while(1){
+        xtimer_periodic_wakeup(&last_wakeup, INTERVAL);
+        puts("Thread B: echo");
+    }
     return NULL;
 }
 
 int main(void)
 {
-    puts("Hello World!");
     puts("Context Switching Task");
 
-    thread_create(thread1_stack, sizeof(thread1_stack),
+    puts("Main: Creating threadA...");
+    thread_create(threadA_stack, sizeof(threadA_stack),
                 THREAD_PRIORITY_MAIN - 2, THREAD_CREATE_STACKTEST,
-                thread1, NULL, "thread1");
+                threadA, NULL, "threadA");
 
-   thread_create(thread2_stack, sizeof(thread2_stack),
+    puts("Main: Creating threadB...");
+   thread_create(threadB_stack, sizeof(threadB_stack),
                THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST,
-               thread2, NULL, "thread2");
+               threadB, NULL, "threadB");
     return 0;
 }
