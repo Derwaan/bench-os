2e36ee459a42eb5d894ca5e19dac82c5106696c4
Trong-Vu <trong-vu.tran@student.uclouvain.be>
2018-12-18 20:50:27 +0100

    First implem of bench framework for demo

--- tasks/riot/demo_remote/main.c
+++ tasks/riot/demo_remote/main.c
@@ -6,6 +6,11 @@
 #include "board.h"
 #include "periph/gpio.h"
 #include "xtimer.h"
+#include "thread.h"
+#include "bench_context_switching.h"
+
+#define THREAD_IDLE 2
+char thread_idle_stack[THREAD_STACKSIZE_MAIN];
 
 #define MAIN_QUEUE_SIZE     (8)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
@@ -16,6 +21,17 @@ static const shell_command_t shell_commands[] = {
     { "udp", "send data over UDP and listen on UDP ports", udp_cmd },
     { NULL, NULL, NULL }
 };
+
+void *thread_idle(void *arg)
+{
+    (void) arg;
+
+    while(1){
+        bench_ping(THREAD_IDLE);
+        // Do nothing
+    }
+    return NULL;
+}
 
 static void cb(void *arg)
 {
@@ -42,6 +58,11 @@ int main(void)
         return 1;
     }
     puts("[SUCCESS] Button initialized!");
+
+
+    thread_create(thread_idle_stack, sizeof(thread_idle_stack),
+                14, THREAD_CREATE_WOUT_YIELD,
+                thread_idle, NULL, "thread_idle");
 
     /* we need a message queue for the thread running the shell in order to
      * receive potentially fast incoming networking packets */
