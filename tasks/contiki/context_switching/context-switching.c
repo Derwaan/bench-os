1011115505fbc64749694bdfbc0398df3d488f94
Gomez Julien <derwaan@gmail.com>
2018-12-20 17:49:24 +0100

    Add test task to contiki for context-switching
    Strange value: display number ticks but get zero ticks between two tasks

--- tasks/contiki/context_switching/context-switching.c
+++ tasks/contiki/context_switching/context-switching.c
@@ -1,17 +1,27 @@
 #include "contiki.h"
+#include "bench-context-switching.h"
 
 #include <stdio.h>
+
+#define THREAD_1 1
+#define THREAD_2 2
 
 PROCESS(task_1, "First task");
 PROCESS(task_2, "Second task");
 AUTOSTART_PROCESSES(&task_1, &task_2);
+
+static struct etimer timer_1;
+static struct etimer timer_2;
 
 PROCESS_THREAD(task_1, ev, data)
 {
     PROCESS_BEGIN();
 
     while (1) {
-        PROCESS_PAUSE();
+        etimer_set(&timer_1, CLOCK_SECOND);
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_1));
+
+        bench_ping(THREAD_1);
         printf("Hello from task 1\n");
     }
 
@@ -24,7 +34,10 @@ PROCESS_THREAD(task_2, ev, data)
     PROCESS_BEGIN();
 
     while (1) {
-        PROCESS_PAUSE();
+        etimer_set(&timer_2, CLOCK_SECOND);
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_2));
+
+        bench_ping(THREAD_2);
         printf("Hello from task 2\n");
     }
 
